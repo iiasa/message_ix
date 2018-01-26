29ce9eeade014eeabd01a619daaa7849b5eccdc2
Matthew Gidden <matthew.gidden@gmail.com>
2018-01-26 11:33:20 +0100

    Ci (#9)
    * first test for ci
    
    * add tutorial tests
    
    * fix dirname in tests
    
    * install ixmp in dockerfile
    
    * trying to fix this..
    
    * trying to see that we got ixmp
    
    * separate command to ls ixmp
    
    * update submodules for 2.0
    
    * add all tutorials to test
    
    * update all tutorials for new framework
    
    * verbose output and printing kernel version
    
    * fix print statements
    
    * dont use namedtemporaryfile on windows..
    
    * reorder base year capacity freezing after activity freezing
    
    * refactor aut single policy plots to show baseline too, also show scenario list
    
    * update multiple policies to use two context managers
    
    * update answers for multiple policies

---
+++ tests/test_tutorials.py
@@ -0,0 +1,77 @@
+import io
+import os
+import subprocess
+import sys
+import tempfile
+import nbformat
+import pytest
+
+from testing_utils import here
+
+ene_path = os.path.join(here, '..', 'tutorial', 'Austrian_energy_system')
+
+# taken from the execellent example here:
+# https://blog.thedataincubator.com/2016/06/testing-jupyter-notebooks/
+
+
+def _notebook_run(path, kernel=None, capsys=None):
+    """Execute a notebook via nbconvert and collect output.
+    :returns (parsed nb object, execution errors)
+    """
+    major_version = sys.version_info[0]
+    kernel = kernel or 'python{}'.format(major_version)
+    if capsys is not None:
+        with capsys.disabled():
+            print('using py version {} with kernerl {}'.format(
+                major_version, kernel))
+    dirname, __ = os.path.split(path)
+    os.chdir(dirname)
+    fname = os.path.join(here, 'test.ipynb')
+    args = [
+        "jupyter", "nbconvert", "--to", "notebook", "--execute",
+        "--ExecutePreprocessor.timeout=60",
+        "--ExecutePreprocessor.kernel_name={}".format(kernel),
+        "--output", fname, path]
+    subprocess.check_call(args)
+
+    nb = nbformat.read(io.open(fname, encoding='utf-8'),
+                       nbformat.current_nbformat)
+
+    errors = [
+        output for cell in nb.cells if "outputs" in cell
+        for output in cell["outputs"] if output.output_type == "error"
+    ]
+
+    os.remove(fname)
+
+    return nb, errors
+
+
+def test_austria(capsys):
+    fname = os.path.join(ene_path, 'austria.ipynb')
+    nb, errors = _notebook_run(fname, capsys=capsys)
+    assert errors == []
+
+
+def test_austria_single_policy():
+    fname = os.path.join(ene_path, 'austria_single_policy.ipynb')
+    nb, errors = _notebook_run(fname)
+    assert errors == []
+
+
+def test_austria_multiple_policies():
+    fname = os.path.join(ene_path, 'austria_multiple_policies.ipynb')
+    nb, errors = _notebook_run(fname)
+    assert errors == []
+
+
+def test_austria_multiple_policies_answers():
+    fname = os.path.join(ene_path, 'austria_multiple_policies-answers.ipynb')
+    nb, errors = _notebook_run(fname)
+    assert errors == []
+
+
+def test_austria_load():
+    fname = os.path.join(ene_path, 'austria-load_ds_only.ipynb')
+    nb, errors = _notebook_run(fname)
+    assert errors == []
---
+++ tests/testing_utils.py
@@ -0,0 +1,55 @@
+import os
+import pytest
+import shutil
+import tempfile
+import ixmp
+
+
+here = os.path.dirname(os.path.realpath(__file__))
+
+
+def tempdir():
+    return os.path.join(tempfile._get_default_tempdir(),
+                        next(tempfile._get_candidate_names()))
+
+
+def create_local_testdb():
+    # copy testdb
+    dst = tempdir()
+    test_props = os.path.join(dst, 'test.properties')
+    src = os.path.join(here, 'testdb')
+    shutil.copytree(src, dst)
+
+    # create properties file
+    fname = os.path.join(here, 'testdb', 'test.properties_template')
+    with open(fname, 'r') as f:
+        lines = f.read()
+        lines = lines.format(here=dst.replace("\\", "/"))
+    with open(test_props, 'w') as f:
+        f.write(lines)
+
+    return test_props
+
+
+@pytest.fixture(scope="session")
+def test_mp():
+    test_props = create_local_testdb()
+
+    # start jvm
+    ixmp.start_jvm()
+
+    # launch Platform and connect to testdb (reconnect if closed)
+    mp = ixmp.Platform(test_props)
+    mp.open_db()
+
+    yield mp
+
+
+@pytest.fixture(scope="session")
+def test_mp_props():
+    test_props = create_local_testdb()
+
+    # start jvm
+    ixmp.start_jvm()
+
+    yield test_props
