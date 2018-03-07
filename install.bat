e1ef62956d8a148311fe3defaf8c8cf724b36e4d
Thomas Zipperle <tzipperle@users.noreply.github.com>
2018-03-07 16:11:45 +0100

    fixed Win install without R (#35)

--- install.bat
+++ install.bat
@@ -4,24 +4,26 @@ set /p MESSAGE_IX=<.foo
 del .foo
 echo %MESSAGE_IX%
 
+echo Python MESSAGE_IX setup
 chdir ixmp
 python setup.py install
+chdir ../
+python setup.py install
+
+echo R MESSAGE_IX setup
 where /q r
 IF ERRORLEVEL 1 (
     ECHO No valid installation of R found, skipped build and installation of rixmp package.
+    @rem set ERRORLEVEL to 0 -> ignore R setup
+    VERIFY
 ) ELSE (
+    chdir ixmp
     rscript rixmp/build_rixmp.R [--verbose]
-)
-chdir ../
-python setup.py install
-where /q r
-IF ERRORLEVEL 1 (
-    ECHO No valid installation of R found, skipped build and installation of message_ix package.
-) ELSE (
+    chdir ../
     rscript rmessageix/build_rmessageix.R [--verbose]
 )
 
-if %errorlevel% neq 0 exit GOTO InstallError
+if %errorlevel% neq 0 GOTO InstallError
 
 setx IXMP_PATH "%MESSAGE_IX%"
 
