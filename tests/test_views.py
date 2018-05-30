1f5310ecee7a2ce16e8029ffac6f2dd76215d4a6
FRICKO Oliver <fricko@iiasa.ac.at>
2018-05-30 14:50:47 +0200

    updated test to check column_style 'raw_mapping'

--- tests/test_views.py
+++ tests/test_views.py
@@ -28,7 +28,7 @@ def test_tec_view_canning_all_tecs(test_mp, view_df):
     exp = view_df.set_index(['technology', 'node', 'par', 'unit', 'commodity/emission',
                              'level', 'mode', 'node I/O', 'year_vtg/year_rel']).astype('float32')
     exp.sort_index(inplace=True)
-    obs = views.tec_view(scen)
+    obs = views.tec_view(scen, column_style='model_mapping')
     obs.sort_index(inplace=True)
     pdt.assert_frame_equal(exp, obs, check_index_type=False)
 
@@ -39,7 +39,7 @@ def test_tec_view_canning_all_tecs_sort_by_par(test_mp, view_df):
            'level', 'mode', 'node I/O', 'year_vtg/year_rel']
     exp = view_df.sort_values(idx).set_index(idx).astype('float32')
     exp.sort_index(inplace=True)
-    obs = views.tec_view(scen, sort_by='par')
+    obs = views.tec_view(scen, sort_by='par', column_style='model_mapping')
     obs.sort_index(inplace=True)
     pdt.assert_frame_equal(exp, obs, check_index_type=False)
 
@@ -48,9 +48,9 @@ def test_tec_view_canning_single_tec(test_mp, view_df):
     scen = test_mp.Scenario(*msg_args)
     tec = 'transport_from_san-diego'
     exp = view_df[view_df['technology'] == tec].set_index(
-        ['technology', 'node', 'par', 'unit', 'commodity/emission', 'level', 'mode', 'node I/O', 'year_vtg/year_rel'])  # .astype('float32')
+        ['technology', 'node', 'par', 'unit', 'commodity/emission', 'level', 'mode', 'node I/O', 'year_vtg/year_rel'])
     exp.sort_index(inplace=True)
-    obs = views.tec_view(scen, tec=tec)
+    obs = views.tec_view(scen, tec=tec, column_style='model_mapping')
     obs.sort_index(inplace=True)
     #assert 0
     pdt.assert_frame_equal(exp, obs, check_index_type=False)
@@ -62,7 +62,7 @@ def test_tec_view_canning_single_par(test_mp, view_df):
     exp = view_df[view_df['par'] == par].set_index(['technology', 'node', 'par', 'unit', 'commodity/emission',
                                                     'level', 'mode', 'node I/O', 'year_vtg/year_rel']).astype('float32')
     exp.sort_index(inplace=True)
-    obs = views.tec_view(scen, par=par)
+    obs = views.tec_view(scen, par=par, column_style='model_mapping')
     obs.sort_index(inplace=True)
     pdt.assert_frame_equal(exp, obs, check_index_type=False)
 
@@ -83,6 +83,31 @@ def test_tec_view_canning_xlsx_idx(test_mp, view_df):
     exp = exp.set_index(['Technology', 'Region', 'Parameter', 'Unit', 'Commodity/Species',
                          'Level', 'Mode', 'Region I/O', 'Vintage/Year Relation']).astype('float32')
     exp.sort_index(inplace=True)
-    obs = views.tec_view(scen, column_style=True)
+    obs = views.tec_view(scen, column_style='file_mapping')
+    obs.sort_index(inplace=True)
+    pdt.assert_frame_equal(exp, obs, check_index_type=False)
+
+
+def test_tec_view_canning_raw_idx(test_mp, view_df):
+    scen = test_mp.Scenario(*msg_args)
+    exp = view_df
+    exp1 = exp[~exp['par'].isin(['input', 'output'])].drop('node I/O', axis=1)
+
+    exp2 = exp[exp['par'] == 'output'].rename(
+        columns={'node I/O': 'node_dest'})
+    exp2['time_dest'] = 'year'
+
+    exp3 = exp[exp['par'] == 'input'].rename(
+        columns={'node I/O': 'node_origin'})
+    exp3['time_origin'] = 'year'
+
+    exp = pd.concat([exp1, exp2, exp3])
+    exp = exp.rename(columns={'commodity/emission': 'commodity',
+                              'year_vtg/year_rel': 'year_vtg', 'node': 'node_loc'}).fillna(' ')
+    exp['time'] = 'year'
+    idx = ['technology', 'node_loc', 'par', 'unit', 'commodity',
+           'level', 'mode', 'node_dest', 'node_origin', 'time', 'time_dest', 'time_origin', 'year_vtg']
+    exp = exp.sort_values(by=idx).set_index(idx).astype('float32')
+    obs = views.tec_view(scen)
     obs.sort_index(inplace=True)
     pdt.assert_frame_equal(exp, obs, check_index_type=False)
