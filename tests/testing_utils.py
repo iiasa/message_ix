dfcd94b096c78794821f54b160399142481b2f4a
Matthew Gidden <matthew.gidden@gmail.com>
2018-06-05 11:16:46 +0200

    rm extra stuff

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -13,24 +13,6 @@ def tempdir():
                         next(tempfile._get_candidate_names()))
 
 
-def create_local_testdb():
-    # copy testdb
-    dst = tempdir()
-    test_props = os.path.join(dst, 'test.properties')
-    src = os.path.join(here, 'testdb')
-    shutil.copytree(src, dst)
-
-    # create properties file
-    fname = os.path.join(here, 'testdb', 'test.properties_template')
-    with open(fname, 'r') as f:
-        lines = f.read()
-        lines = lines.format(here=dst.replace("\\", "/"))
-    with open(test_props, 'w') as f:
-        f.write(lines)
-
-    return test_props
-
-
 @pytest.fixture(scope="session")
 def test_mp():
     # start jvm
@@ -41,26 +23,3 @@ def test_mp():
     mp.open_db()
 
     yield mp
-
-# @pytest.fixture(scope="session")
-# def test_mp():
-#     test_props = create_local_testdb()
-
-#     # start jvm
-#     ixmp.start_jvm()
-
-#     # launch Platform and connect to testdb (reconnect if closed)
-#     mp = ixmp.Platform(test_props)
-#     mp.open_db()
-
-#     yield mp
-
-
-@pytest.fixture(scope="session")
-def test_mp_props():
-    test_props = create_local_testdb()
-
-    # start jvm
-    ixmp.start_jvm()
-
-    yield test_props
