00c9a0bef4214522ca02440f1e3898f6c610d900
adrivinca <adrivinca@yahoo.it>
2018-06-20 18:39:54 +0200

    adapt to changes rmessage_ix and  reticulate tutorial (#42)

--- rmessageix/source/R/init_rmessageix.R
+++ rmessageix/source/R/init_rmessageix.R
@@ -25,10 +25,10 @@ utils::globalVariables(c("ixmp_path"))
     warning("Check MESSAGE model installation")
   
   # careful to have all the slashes in the same direction
-  model_file = gsub("/","\\\\" ,file.path( paste(ixmp_path, "\\model", sep = '') , "{model}_run.gms" ) )
-  inp = gsub("/","\\\\" , file.path( paste(ixmp_path, "\\model\\data", sep = '') , "MsgData_{case}.gdx" ) )
-  outp = gsub("/","\\\\" , file.path( paste(ixmp_path, "\\model\\output", sep = '') , "MsgOutput_{case}.gdx" ) )
-  iter_file = gsub("/","\\\\" , file.path( paste(ixmp_path, "\\model\\output", sep = '') , "MsgIterationReport_{case}.gdx" ) )
+  model_file = gsub("/","\\\\" ,file.path( paste(ixmp_path, "\\message_ix\\model", sep = '') , "{model}_run.gms" ) )
+  inp = gsub("/","\\\\" , file.path( paste(ixmp_path, "\\message_ix\\model\\data", sep = '') , "MsgData_{case}.gdx" ) )
+  outp = gsub("/","\\\\" , file.path( paste(ixmp_path, "\\message_ix\\model\\output", sep = '') , "MsgOutput_{case}.gdx" ) )
+  iter_file = gsub("/","\\\\" , file.path( paste(ixmp_path, "\\message_ix\\model\\output", sep = '') , "MsgIterationReport_{case}.gdx" ) )
   solve_args = paste("--in=",inp," --out=",outp," --iter=", iter_file, sep = '')
   
   for (i in c("MESSAGE","MESSAGE-MACRO")) {
