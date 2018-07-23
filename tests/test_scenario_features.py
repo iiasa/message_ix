ef504a72607add92de92ba6124ae3a49d6903578
Matthew Gidden <matthew.gidden@gmail.com>
2018-07-23 17:21:53 +0200

    add docs and lower share bound with test

--- tests/test_scenario_features.py
+++ tests/test_scenario_features.py
@@ -68,7 +68,7 @@ def test_add_bound_activity_up_all_modes(test_mp):
     assert np.isclose(obs, exp)
 
 
-def test_add_share_output(test_mp):
+def test_add_share_output_up(test_mp):
     scen = Scenario(test_mp, *msg_args)
     scen.solve()
 
@@ -100,6 +100,52 @@ def test_add_share_output(test_mp):
                       'type_tec_total': 'total',
                   }, index=[0]))
     clone.add_par('share_factor_up',
+                  pd.DataFrame({
+                      'shares': 'test-share',
+                      'node_loc': 'new-york',
+                      'year_act': 2010,
+                      'time': 'year',
+                      'unit': 'cases',
+                      'value': exp,
+                  }, index=[0]))
+    clone.commit('foo')
+    clone.solve()
+    obs = calc_share(clone)
+    assert np.isclose(obs, exp)
+
+
+def test_add_share_output_lo(test_mp):
+    scen = Scenario(test_mp, *msg_args)
+    scen.solve()
+
+    # data for share bound
+    def calc_share(s):
+        a = calculate_activity(s, city='seattle').loc['to_new-york']
+        b = calculate_activity(s, city='san-diego').loc['to_new-york']
+        return a / (a + b)
+
+    exp = 1.05 * calc_share(scen)
+
+    # add share constraints
+    clone = scen.clone('foo', 'baz', keep_sol=False)
+    clone.check_out()
+    clone.add_set('type_tec', ['share', 'total'])
+    cat_tec = [
+        ['share', 'transport_from_seattle'],
+        ['total', 'transport_from_seattle'],
+        ['total', 'transport_from_san-diego'],
+    ]
+    clone.add_set('cat_tec', cat_tec)
+    clone.add_set('shares', 'test-share')
+    clone.add_set('map_shares_commodity_level',
+                  pd.DataFrame({
+                      'shares': 'test-share',
+                      'commodity': 'cases',
+                      'level': 'consumption',
+                      'type_tec_share': 'share',
+                      'type_tec_total': 'total',
+                  }, index=[0]))
+    clone.add_par('share_factor_lo',
                   pd.DataFrame({
                       'shares': 'test-share',
                       'node_loc': 'new-york',
