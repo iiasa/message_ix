9fd4e773b6b160f94a85890275f203618ba27e39
Matthew Gidden <matthew.gidden@gmail.com>
2018-07-23 14:46:36 +0200

    initial testsing

--- tests/test_scenario_features.py
+++ tests/test_scenario_features.py
@@ -6,13 +6,13 @@ from message_ix import Scenario
 msg_args = ('canning problem (MESSAGE scheme)', 'standard')
 
 
-def calculate_seattle_activity(scen):
+def calculate_activity(scen, city='seattle'):
     return (
         scen
         .var('ACT')
         .groupby(['technology', 'mode'])['lvl']
         .sum()
-        .loc['transport_from_seattle']
+        .loc['transport_from_' + city]
     )
 
 
@@ -21,7 +21,7 @@ def test_add_bound_activity_up(test_mp):
     scen.solve()
 
     # data for act bound
-    exp = 0.5 * calculate_seattle_activity(scen).sum()
+    exp = 0.5 * calculate_activity(scen).sum()
     data = pd.DataFrame({
         'node_loc': 'seattle',
         'technology': 'transport_from_seattle',
@@ -38,7 +38,7 @@ def test_add_bound_activity_up(test_mp):
     clone.add_par('bound_activity_up', data)
     clone.commit('foo')
     clone.solve()
-    obs = calculate_seattle_activity(clone).loc['to_chicago']
+    obs = calculate_activity(clone).loc['to_chicago']
     assert np.isclose(obs, exp)
 
 
@@ -47,7 +47,7 @@ def test_add_bound_activity_up_all_modes(test_mp):
     scen.solve()
 
     # data for act bound
-    exp = 0.95 * calculate_seattle_activity(scen).sum()
+    exp = 0.95 * calculate_activity(scen).sum()
     data = pd.DataFrame({
         'node_loc': 'seattle',
         'technology': 'transport_from_seattle',
@@ -64,5 +64,47 @@ def test_add_bound_activity_up_all_modes(test_mp):
     clone.add_par('bound_activity_up', data)
     clone.commit('foo')
     clone.solve()
-    obs = calculate_seattle_activity(clone).sum()
+    obs = calculate_activity(clone).sum()
     assert np.isclose(obs, exp)
+
+
+def test_add_share_output(test_mp):
+    msg_multiyear_args = ('canning problem (MESSAGE scheme)', 'multi-year')
+    scen = Scenario(test_mp, *msg_multiyear_args)
+    scen.solve()
+    print(scen.par('output'))
+    print(scen.var('ACT'))
+    print(scen.set('type_tec'))
+
+    # data for share bound
+    print(calculate_activity(scen, city='seattle'))
+    print(calculate_activity(scen, city='san-diego'))
+
+    def calc_share(scen):
+        a = calculate_activity(scen, city='seattle').loc['to_new-york']
+        b = calculate_activity(scen, city='san-diego').loc['to_new-york']
+        return a / (a + b)
+
+    exp = 0.95 * calc_share(scen)
+    print(exp)
+    data = pd.DataFrame({
+        'node_loc': 'new-york',
+        'type_tec_numerator': 'transport_from_seattle',
+        'type_tec_denominator': ['transport_from_seattle', 'transport_from_san-diego'],
+        'level': 'consumption',
+        'year_act': 2010,
+        'time': 'year',
+        'unit': 'cases',
+        'value': exp,
+    })  # , index=[0])
+
+    print(data)
+
+    # # test limiting all modes
+    # clone = scen.clone('foo', 'baz', keep_sol=False)
+    # clone.check_out()
+    # clone.add_par('bound_activity_up', data)
+    # clone.commit('foo')
+    # clone.solve()
+    # obs = calculate_activity(clone).sum()
+    # assert np.isclose(obs, exp)
