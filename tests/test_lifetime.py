4619a49e65e8ccec25c377a75f0d81684f22f3bc
Unknown <behnam.zakeri@live.com>
2018-12-12 11:29:15 +0100

    test file updated to read data from an existing scenario

--- tests/test_lifetime.py
+++ tests/test_lifetime.py
@@ -1,149 +1,99 @@
 # -*- coding: utf-8 -*-
 # Importing required packages
+
+import os
 import ixmp as ix
+from message_ix import Scenario
 import pandas as pd
 from itertools import product
 from pandas.testing import assert_frame_equal
+from conftest import here
+
+db_dir = os.path.join(here, 'testdb')
+test_db = os.path.join(db_dir, 'ixmptest')
+test_mp = ix.Platform(dbprops=test_db, dbtype='HSQLDB')
+
+file_path = os.path.join(here, '..', 'toolbox', 'lifetime_changer')
+os.chdir(file_path)
 from f_lifetimeChanger import change_lifetime
 
-# Building a new scenario and adding two parameters
-mp = ix.Platform()
-scen = mp.Scenario(
-    'model_test',
-    'scenario_test',
-    version='new',
-    scheme='MESSAGE')
 
-scen.add_set('technology', 'tech_test')
-scen.add_set('node', 'node_test')
-scen.add_set('commodity', 'com_test')
-scen.add_set('mode', 'M_test')
-scen.add_set('level', 'level_test')
-horizon = list(range(1990, 2081, 10))
-lifetime = 30
-scen.add_set('year', horizon)
-scen.add_set('cat_year', pd.DataFrame(
-    {'type_year': 'firstmodelyear', 'year': '2020'}, index=[0]))
+def test_lifetime_changer(test_mp):
+    msg_multiyear_args = ('canning problem (MESSAGE scheme)', 'multi-year')
+    scen = Scenario(test_mp, *msg_multiyear_args)
+    assert scen.par('technical_lifetime')[
+        'technology'].isin(['canning_plant']).any()
+    scen.solve()
 
-first_year_vtg = 2010
-df = pd.DataFrame({'node_loc': 'node_test', 'technology': 'tech_test', 'year_vtg': [
-                  x for x in horizon if x >= first_year_vtg], 'unit': 'y', 'value': lifetime})
+    df_tec = scen.par(
+        'technical_lifetime', {
+            'node_loc': 'seattle', 'technology': 'canning_plant'})
+    df_par = scen.par(
+        'output', {
+            'node_loc': 'seattle', 'technology': 'canning_plant'})
 
-scen.add_par('technical_lifetime', df)
-years_pair = [
-    x for x in product(
-        horizon,
-        horizon) if x[1] >= x[0] and x[1] -
-    x[0] < lifetime]
-df = pd.DataFrame({'node_loc': 'node_test',
-                   'node_dest': 'node_test',
-                   'technology': 'tech_test',
-                   'time': 'year',
-                   'time_dest': 'year',
-                   'commodity': 'com_test',
-                   'level': 'level_test',
-                   'mode': 'M_test',
-                   'unit': '-',
-                   'year_vtg': [x[0] for x in years_pair if x[0] >= first_year_vtg],
-                   'year_act': [x[1] for x in years_pair if x[0] >= first_year_vtg],
-                   'value': 1})
-scen.add_par('output', df)
-scen.commit('test built')
+    # Building a new scenario and adding two parameters
+    clone = scen.clone('foo', 'bar', keep_sol=False)
+    clone.check_out()
+    # Changing the lifetime based on avaiable input and output
+    df_tec.loc[df_tec['technology'] == 'canning_plant', 'value'] = 10
+    clone.add_par('technical_lifetime', df_tec)
+    clone.commit('')
+    # Testing the function change_lifetime
+    lifetime_new = 20
+    year_vtg_start = 2020
+    year_vtg_end = 2030
 
-# Testing the function change_lifetime
-# 1) Testing the case of lifetime extension
-lifetime_new = 40
-year_start = 1990
+    dfs = change_lifetime(
+        test_mp,
+        clone,
+        'canning_plant',
+        lifetime_new,
+        year_vtg_start,
+        year_vtg_end,
+        year_act_end=None,
+        nodes='seattle',
+        par_exclude=None,
+        remove_old=False,
+        test_run=False,
+        extrapol_neg=0.5)
 
-dfs = change_lifetime(
-    scen,
-    'tech_test',
-    lifetime_new,
-    year_start,
-    year_end=None,
-    nodes=all,
-    par_exclude=None,
-    remove_old=False,
-    test_run=False,
-    extrapol_neg=0.5)
+    # Testing parameter "technical_lifetime"
+    df_test = df_tec.copy()
+    horizon = [int(x) for x in clone.set('year')]
+    df_test.loc[df_test['technology'] ==
+                'canning_plant', 'value'] = lifetime_new
+    if year_vtg_end > max(df_tec['year_vtg']):
+        df_row = df_test.loc[0, :].copy()
+        for yr in [x for x in horizon if x > max(
+                df_tec['year_vtg']) and x <= year_vtg_end]:
+            df_row['year_vtg'] = yr
+            df_test = df_test.append(df_row, ignore_index=True)
 
-# Testing parameter "technical_lifetime"
-df_test = pd.DataFrame({'node_loc': 'node_test',
-                        'technology': 'tech_test',
-                        'year_vtg': [x for x in horizon if x >= year_start],
-                        'unit': 'y',
-                        'value': lifetime_new})
-assert_frame_equal(df_test, dfs['technical_lifetime'], check_dtype=False)
+    assert_frame_equal(df_test, dfs['technical_lifetime'], check_dtype=False)
 
-# Testing parameter "output"
-years_pair_test = [
-    x for x in product(
-        horizon,
-        horizon) if x[1] >= x[0] and x[1] -
-    x[0] < lifetime_new]
-df_test = pd.DataFrame({'node_loc': 'node_test',
-                        'node_dest': 'node_test',
-                        'technology': 'tech_test',
-                        'time': 'year',
-                        'time_dest': 'year',
-                        'commodity': 'com_test',
-                        'level': 'level_test',
-                        'mode': 'M_test',
-                        'unit': '-',
-                        'year_vtg': [x[0] for x in years_pair_test if x[0] >= year_start],
-                        'year_act': [x[1] for x in years_pair_test if x[0] >= year_start],
-                        'value': 1}).sort_values(['year_vtg',
-                                                  'year_act']).reset_index(drop=True)
+    # Testing parameter "output"
+    df_test2 = pd.DataFrame(columns=df_par.columns)
+    df_test2 = df_par.copy()
+    years_pair_test = [
+        x for x in product(
+            horizon,
+            horizon) if x[1] >= x[0] and x[1] -
+        x[0] < lifetime_new and x[0] >= min(
+            df_test['year_vtg']) and x[0] <= max(
+                df_test['year_vtg'])]
 
-df_new = scen.par('output').sort_values(
-    ['year_vtg', 'year_act']).reset_index(drop=True)
-assert_frame_equal(df_test, df_new, check_like=True, check_dtype=False)
+    df_row = df_par.loc[0, :].copy()
+    for x in years_pair_test:
+        df_row['year_vtg'] = x[0]
+        df_row['year_act'] = x[1]
+        df_test2 = df_test2.append(df_row, ignore_index=True)
 
-# 2) Testing the case of lifetime contraction
+    df_new = clone.par('output',
+                       {'node_loc': 'seattle',
+                        'technology': 'canning_plant'}).sort_values(['year_vtg',
+                                                                     'year_act']).reset_index(drop=True)
 
-lifetime_new = 25
-year_start = 2020
-year_end = 2070
+    assert_frame_equal(df_test2, df_new, check_like=True, check_dtype=False)
+    clone.solve()
 
-dfs = change_lifetime(
-    scen,
-    'tech_test',
-    lifetime_new,
-    year_start,
-    year_end,
-    nodes=all,
-    par_exclude=None,
-    remove_old=True,
-    test_run=False,
-    extrapol_neg=0.5)
-
-# Testing parameter "technical_lifetime"
-df_test = pd.DataFrame({'node_loc': 'node_test', 'technology': 'tech_test',
-                        'year_vtg': [x for x in horizon if x >= year_start and x <= year_end],
-                        'unit': 'y', 'value': lifetime_new})
-assert_frame_equal(df_test, dfs['technical_lifetime'], check_dtype=False)
-
-# Testing parameter "output"
-period_test = [x for x in horizon if x >= year_start and x <= year_end]
-years_pair_test = [
-    x for x in product(
-        period_test,
-        period_test) if x[1] >= x[0] and x[1] -
-    x[0] < lifetime_new]
-df_test = pd.DataFrame({'node_loc': 'node_test',
-                        'node_dest': 'node_test',
-                        'technology': 'tech_test',
-                        'time': 'year',
-                        'time_dest': 'year',
-                        'commodity': 'com_test',
-                        'level': 'level_test',
-                        'mode': 'M_test',
-                        'unit': '-',
-                        'year_vtg': [x[0] for x in years_pair_test],
-                        'year_act': [x[1] for x in years_pair_test],
-                        'value': 1}).sort_values(['year_vtg',
-                                                  'year_act']).reset_index(drop=True)
-
-df_new = scen.par('output').sort_values(
-    ['year_vtg', 'year_act']).reset_index(drop=True)
-assert_frame_equal(df_test, df_new, check_like=True, check_dtype=False)
