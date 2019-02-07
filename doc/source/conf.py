83219ff93115b89a99861c165840bc711c2aa896
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-02-07 14:20:44 +0100

    add IIASA white logo & change sidebar background

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -145,7 +145,7 @@ html_theme_options = {}
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/IIASA_logo.png'
+html_logo = '_static/logo_white.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -156,6 +156,10 @@ html_logo = '_static/IIASA_logo.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+def setup(app):
+    app.add_stylesheet('custom.css')
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
