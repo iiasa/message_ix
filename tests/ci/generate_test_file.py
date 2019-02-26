f80ad367c17c303512710d654c6743c8567e5028
Matthew Gidden <matthew.gidden@gmail.com>
2019-02-26 09:32:58 +0100

    update test generation for multiple cases per scenario
    update readme for env vars
    
    update some scenci files
    
    add solve options to ci tests

--- tests/ci/generate_test_file.py
+++ tests/ci/generate_test_file.py
@@ -9,30 +9,42 @@ import numpy as np
 from paths import dbpath
 """
 
-template = """
+setup_template = """
 def test_{name}():
     mp = ixmp.Platform(dbpath, dbtype='HSQLDB')
     scen = message_ix.Scenario(mp, '{model}', '{scenario}')
-    scen.solve(model='{solve}')
+    scen.solve(model='{solve}', solve_options={solve_options})
+"""
+
+test_template = """
     obs = {obs}
     exp = {exp}
     {test}
 """
 
 
+def query(d, key, default):
+    return d[key] if key in d else default
+
+
 def main():
     text = prefix
     with open('scenarios.yaml', 'r') as f:
         for name, data in yaml.load(f).items():
-            text += template.format(
+            solve_options = query(data, 'solve_options', {})
+            text += setup_template.format(
                 name=name,
                 model=data['model'],
                 scenario=data['scenario'],
                 solve=data['solve'],
-                obs=data['obs'],
-                exp=data['exp'],
-                test=data['test'],
+                solve_options=solve_options,
             )
+            for case in data['cases']:
+                text += test_template.format(
+                    obs=case['obs'],
+                    exp=case['exp'],
+                    test=case['test'],
+                )
     with open('test_scenarios.py', 'w') as f:
         f.write(text)
 
