cf741e2e1b12eb44c7b3831cfbcf763ae3814e64
Daniel Huppmann <huppmann@iiasa.ac.at>
2019-03-14 14:49:20 +0100

    adding docstrings to explain the assertions

--- tests/test_feature_price_emission.py
+++ tests/test_feature_price_emission.py
@@ -4,13 +4,13 @@ import numpy.testing as npt
 MODEL = 'test_emissions_price'
 
 
-def model_setup(scen, years):
+def model_setup(scen, years, simple_tecs=True):
+    """generate a minimal model to test the behaviour of the emission prices"""
     scen.add_spatial_sets({'country': 'node'})
     scen.add_set('commodity', 'comm')
     scen.add_set('level', 'level')
     scen.add_set('year', years)
 
-    scen.add_set('technology', ['dirty_tec', 'clean_tec'])
     scen.add_set('mode', 'mode')
 
     scen.add_set('emission', 'co2')
@@ -20,6 +20,14 @@ def model_setup(scen, years):
         scen.add_par('interestrate', y, 0.05, '-')
         scen.add_par('demand', ['node', 'comm', 'level', y, 'year'], 1, 'GWa')
 
+    if simple_tecs:
+        add_two_tecs(scen, years)
+
+
+def add_two_tecs(scen, years):
+    """add two technologies to the scenario"""
+    scen.add_set('technology', ['dirty_tec', 'clean_tec'])
+    for y in years:
         # the dirty technology is free (no costs) but has emissions
         output_specs = ['node', 'comm', 'level', 'year', 'year']
         tec_specs = ['node', 'dirty_tec', y, y, 'mode']
@@ -38,7 +46,9 @@ def test_no_constraint(test_mp):
     scen.commit('initialize test scenario')
     scen.solve()
 
+    # without emissions constraint, the zero-cost technology satisfies demand
     assert scen.var('OBJ')['lvl'] == 0
+    # without emissions constraint, there are no emission prices
     assert scen.var('PRICE_EMISSION').empty
 
 
@@ -53,7 +63,10 @@ def test_cumulative_equidistant(test_mp):
     scen.commit('initialize test scenario')
     scen.solve()
 
+    # with emissions constraint, the technology with costs satisfies demand
     assert scen.var('OBJ')['lvl'] > 0
+    # under a cumulative constraint, the price must increase with the discount
+    # rate starting from the marginal relaxation in the first year
     obs = scen.var('PRICE_EMISSION')['lvl'].values
     npt.assert_allclose(obs, [1.05**(y - years[0]) for y in years])
 
@@ -70,7 +83,10 @@ def test_per_period_equidistant(test_mp):
     scen.commit('initialize test scenario')
     scen.solve()
 
+    # with emissions constraint, the technology with costs satisfies demand
     assert scen.var('OBJ')['lvl'] > 0
+    # under per-year emissions constraints, the emission price must be equal to
+    # the marginal relaxation, ie. the difference in costs between technologies
     obs = scen.var('PRICE_EMISSION')['lvl']
     npt.assert_allclose(obs, [1] * 3)
 
@@ -86,7 +102,10 @@ def test_cumulative_variable_periodlength(test_mp):
     scen.commit('initialize test scenario')
     scen.solve()
 
+    # with an emissions constraint, the technology with costs satisfies demand
     assert scen.var('OBJ')['lvl'] > 0
+    # under a cumulative constraint, the price must increase with the discount
+    # rate starting from the marginal relaxation in the first year
     obs = scen.var('PRICE_EMISSION')['lvl'].values
     npt.assert_allclose(obs, [1.05**(y - years[0]) for y in years])
 
@@ -103,7 +122,10 @@ def test_per_period_variable_periodlength(test_mp):
     scen.commit('initialize test scenario')
     scen.solve()
 
+    # with an emissions constraint, the technology with costs satisfies demand
     assert scen.var('OBJ')['lvl'] > 0
+    # under per-year emissions constraints, the emission price must be equal to
+    # the marginal relaxation, ie. the difference in costs between technologies
     obs = scen.var('PRICE_EMISSION')['lvl'].values
     npt.assert_allclose(obs, [1] * 4)
 
@@ -121,6 +143,9 @@ def test_custom_type_variable_periodlength(test_mp):
     scen.commit('initialize test scenario')
     scen.solve()
 
+    # with an emissions constraint, the technology with costs satisfies demand
     assert scen.var('OBJ')['lvl'] > 0
+    # under a cumulative constraint, the price must increase with the discount
+    # rate starting from the marginal relaxation in the first year
     obs = scen.var('PRICE_EMISSION')['lvl'].values
     npt.assert_allclose(obs, [1.05**(y - custom[0]) for y in custom])
