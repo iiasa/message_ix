a3f048d0d73e1cfc5b81bebdfce17d85891bd52a
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-05-07 20:29:42 +0200

    Add initial tests

--- message_ix/reporting.py
+++ message_ix/reporting.py
@@ -1,10 +1,44 @@
 from ixmp.reporting import Reporter as IXMPReporter
+from ixmp.reporting.utils import ureg, rename_dims
+
+
+# Adjust the ixmp default reporting for MESSAGEix
+
+# Units appearing in MESSAGEix test data
+ureg.define('y = year')
+
+# Short names for dimensions
+rename_dims.update({
+    # As defined in the documentation
+    'commodity': 'c',
+    'emission': 'e',
+    'grade': 'g',
+    'land_scenario': 's',
+    'land_type': 'u',
+    'level': 'l',
+    'mode': 'm',
+    'node': 'n',
+    'rating': 'q',
+    'relation': 'r',
+    'technology': 't',
+    'time': 'h',
+    'year': 'y',
+    # New
+    'node_dest': 'nd',
+    'node_loc': 'nl',
+    'node_origin': 'no',
+    'node_rel': 'nr',
+    'node_share': 'ns',
+    'time_dest': 'hd',
+    'time_origin': 'ho',
+    'year_act': 'ya',
+    'year_vtg': 'yv',
+    'year_rel': 'yr',
+})
 
 
 class Reporter(IXMPReporter):
-    """MESSAGEix Reporter.
-
-    """
+    """MESSAGEix Reporter."""
     def __init__(self):
         # TODO add MESSAGE_ix specific nodes from a file
-        super(Reporter, self).__init__(self)
+        super().__init__()
---
+++ tests/test_reporting.py
@@ -0,0 +1,31 @@
+import xarray as xr
+from xarray.testing import assert_equal as assert_xr_equal
+
+from message_ix import Scenario
+from message_ix.reporting import Reporter
+
+
+def test_reporter(test_mp):
+    scen = Scenario(test_mp,
+                    'canning problem (MESSAGE scheme)',
+                    'standard')
+
+    # Reporter can be initialized
+    rep = Reporter.from_scenario(scen)
+
+    # Number of quantities available in a rudimentary MESSAGEix Scenario
+    assert len(rep.graph['all']) == 101
+
+    # Quantities have short dimension names
+    assert 'demand:n-c-l-y-h' in rep.graph
+
+    # Aggregates are available
+    assert len(rep.graph) == 2689
+    assert 'demand:n-l-h' in rep.graph
+
+    # Quantities are available
+    dims = dict(coords=['chicago new-york topeka'.split()], dims=['n'])
+    demand = xr.DataArray([300, 325, 275], **dims)
+
+    # NB the call to squeeze() drops the length-1 dimensions c-l-y-h
+    assert_xr_equal(rep.get('demand:n-c-l-y-h').squeeze(drop=True), demand)
