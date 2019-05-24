0429dd78a046ae16f7aff08f4bec2f00169494ae
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-05-24 11:27:47 +0200

    Update conftest and fixture usage

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,43 +1,70 @@
 import os
-import pytest
-import shutil
-import tempfile
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path
 
 import ixmp
+from ixmp.testing import create_local_testdb
+import message_ix
+import pytest
 
 
-here = os.path.dirname(os.path.realpath(__file__))
+# pytest hooks
+
+def pytest_report_header(config, startdir):
+    """Add the message_ix import path to the pytest report header."""
+    return 'message_ix location: {}'.format(Path(message_ix.__file__).parent)
 
 
-def tempdir():
-    return os.path.join(tempfile._get_default_tempdir(),
-                        next(tempfile._get_candidate_names()))
+def pytest_sessionstart(session):
+    """Unset any configuration read from the user's directory."""
+    ixmp.config._config.clear()
+    print(ixmp.config._config.values)
 
 
-def create_local_testdb():
-    # copy testdb
-    dst = tempdir()
-    test_props = os.path.join(dst, 'test.properties')
-    src = os.path.join(here, 'testdb')
-    shutil.copytree(src, dst)
+# Fixtures
 
-    # create properties file
-    fname = os.path.join(here, 'testdb', 'test.properties_template')
-    with open(fname, 'r') as f:
-        lines = f.read()
-        lines = lines.format(here=dst.replace("\\", "/"))
-    with open(test_props, 'w') as f:
-        f.write(lines)
+@pytest.fixture(scope='session')
+def test_data_path(request):
+    """Path to the directory containing test data."""
+    return Path(__file__).parent / 'data'
 
-    return test_props
+
+@pytest.fixture
+def tutorial_path(request):
+    """Path to the directory containing tutorials."""
+    return Path(__file__).parent / '..' / 'tutorial'
+
+
+@pytest.fixture(scope='session')
+def tmp_env(tmp_path_factory):
+    """Return the os.environ dict with the IXMP_DATA variable set.
+
+    IXMP_DATA will point to a temporary directory that is unique to the
+    test session. ixmp configuration (i.e. the 'config.json' file) can be
+    written and read in this directory without modifying the current user's
+    configuration.
+    """
+    os.environ['IXMP_DATA'] = str(tmp_path_factory.mktemp('config'))
+
+    yield os.environ
 
 
 @pytest.fixture(scope="session")
-def test_mp():
-    test_props = create_local_testdb()
+def test_mp(tmp_path_factory, test_data_path):
+    """An ixmp.Platform connected to a temporary, local database.
 
-    # start jvm
-    ixmp.start_jvm()
+    *test_mp* is used across the entire test session, so the contents of the
+    database may reflect other tests already run.
+    """
+    # casting to Path(str()) is a hotfix due to errors upstream in pytest on
+    # Python 3.5 (at least, perhaps others), there is an implicit cast to
+    # python2's pathlib which is incompatible with python3's pathlib Path
+    # objects.  This can be taken out once it is resolved upstream and CI is
+    # setup on multiple Python3.x distros.
+    db_path = Path(str(tmp_path_factory.mktemp('test_mp')))
+    test_props = create_local_testdb(db_path, test_data_path / 'testdb')
 
     # launch Platform and connect to testdb (reconnect if closed)
     mp = ixmp.Platform(test_props)
@@ -47,10 +74,14 @@ def test_mp():
 
 
 @pytest.fixture(scope="session")
-def test_mp_props():
-    test_props = create_local_testdb()
-
-    # start jvm
-    ixmp.start_jvm()
+def test_mp_props(tmp_path_factory, test_data_path):
+    """Path to a database properties file referring to a test database."""
+    # casting to Path(str()) is a hotfix due to errors upstream in pytest on
+    # Python 3.5 (at least, perhaps others), there is an implicit cast to
+    # python2's pathlib which is incompatible with python3's pathlib Path
+    # objects.  This can be taken out once it is resolved upstream and CI is
+    # setup on multiple Python3.x distros.
+    db_path = Path(str(tmp_path_factory.mktemp('test_mp_props')))
+    test_props = create_local_testdb(db_path, test_data_path / 'testdb')
 
     yield test_props
--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -1,176 +1,67 @@
-import io
-import os
-import subprocess
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path
 import sys
+
+from ixmp.testing import run_notebook, get_cell_output
+import numpy as np
 import pytest
 
-import numpy as np
 
-from conftest import here
-
-is_py2 = sys.version_info[0] == 2
-
-try:
-    import nbformat
-    jupyter_installed = True
-except ImportError:
-    jupyter_installed = False
-
-ene_path = os.path.join(here, '..', 'tutorial', 'Austrian_energy_system')
-westeros_path = os.path.join(here, '..', 'tutorial', 'westeros')
-
-py2_deprecated = 'Python 2 is deprecated in the tutorials'
-jupyter_required = 'requires Jupyter Notebook to be installed'
-
-# taken from the execellent example here:
-# https://blog.thedataincubator.com/2016/06/testing-jupyter-notebooks/
+# Skip the entire file on Python 2
+if sys.version_info[0] == 2:
+    py2_deprecated = 'Python 2 is deprecated in the tutorials'
+    pytestmark = pytest.mark.skip(reason=py2_deprecated)
 
 
-def _notebook_run(path, kernel=None, capsys=None):
-    """Execute a notebook via nbconvert and collect output.
-    :returns (parsed nb object, execution errors)
-    """
-    major_version = sys.version_info[0]
-    kernel = kernel or 'python{}'.format(major_version)
-    dirname, __ = os.path.split(path)
-    os.chdir(dirname)
-    fname = os.path.join(here, 'test.ipynb')
-    args = [
-        "jupyter", "nbconvert", "--to", "notebook", "--execute",
-        "--ExecutePreprocessor.timeout=60",
-        "--ExecutePreprocessor.kernel_name={}".format(kernel),
-        "--output", fname, path]
-    subprocess.check_call(args)
+AT = 'Austrian_energy_system'
 
-    nb = nbformat.read(io.open(fname, encoding='utf-8'),
-                       nbformat.current_nbformat)
-
-    errors = [
-        output for cell in nb.cells if "outputs" in cell
-        for output in cell["outputs"] if output.output_type == "error"
-    ]
-
-    os.remove(fname)
-
-    return nb, errors
-
-
-def get_cell_by_name(nb, name):
-    """Retrieve a cell from *nb* according to its metadata *name*:
-
-    The Jupyter notebook format allows specifying a document-wide unique 'name'
-    metadata attribute for each cell:
-
-    https://nbformat.readthedocs.io/en/latest/format_description.html
-                                                                #cell-metadata
-
-    Return the cell matching the name, or raise ValueError.
-    """
-    for i, cell in enumerate(nb.cells):
-        try:
-            cell_name = cell.metadata.jupyter.name
-            if cell_name == name:
-                return cell
-        except AttributeError:
-            continue
-
-    raise ValueError("no cell named '{}'".format(name))
-
-
-@pytest.mark.skipif(is_py2, reason=py2_deprecated)
-@pytest.mark.skipif(not jupyter_installed, reason=jupyter_required)
-def test_westeros_baseline(capsys):
-    fname = os.path.join(westeros_path, 'westeros_baseline.ipynb')
-    nb, errors = _notebook_run(fname, capsys=capsys)
-    assert errors == []
-
-    # On Python 2, the returned objective value is different
-    if sys.version_info[0] == 3:
-        return
-
-    cell = get_cell_by_name(nb, 'solve-objective-value')
-    obs = eval(cell['outputs'][0]['data']['text/plain'])
-    exp = 187445.953125
-    assert np.isclose(obs, exp)
-
-
-@pytest.mark.skipif(is_py2, reason=py2_deprecated)
-@pytest.mark.skipif(not jupyter_installed, reason=jupyter_required)
-def test_westeros_emissions(capsys):
-    fname = os.path.join(westeros_path, 'westeros_emissions_bounds.ipynb')
-    nb, errors = _notebook_run(fname, capsys=capsys)
-    assert errors == []
-
-
-@pytest.mark.skipif(is_py2, reason=py2_deprecated)
-@pytest.mark.skipif(not jupyter_installed, reason=jupyter_required)
-def test_westeros_emissions_tax(capsys):
-    fname = os.path.join(westeros_path, 'westeros_emissions_taxes.ipynb')
-    nb, errors = _notebook_run(fname, capsys=capsys)
-    assert errors == []
-
-
-@pytest.mark.skipif(is_py2, reason=py2_deprecated)
-@pytest.mark.skipif(not jupyter_installed, reason=jupyter_required)
-def test_westeros_firm_capacity(capsys):
-    fname = os.path.join(westeros_path, 'westeros_firm_capacity.ipynb')
-    nb, errors = _notebook_run(fname, capsys=capsys)
-    assert errors == []
-
-
-@pytest.mark.skipif(is_py2, reason=py2_deprecated)
-@pytest.mark.skipif(not jupyter_installed, reason=jupyter_required)
-def test_westeros_flexible_generation(capsys):
-    fname = os.path.join(westeros_path, 'westeros_flexible_generation.ipynb')
-    nb, errors = _notebook_run(fname, capsys=capsys)
-    assert errors == []
-
-
-@pytest.mark.skipif(is_py2, reason=py2_deprecated)
-@pytest.mark.skipif(not jupyter_installed, reason=jupyter_required)
-def test_austria(capsys):
-    fname = os.path.join(ene_path, 'austria.ipynb')
-    nb, errors = _notebook_run(fname, capsys=capsys)
-    assert errors == []
-
+# Argument values to parametrize test_tutorial
+# Each item is a 3-tuple:
+# 1. Path fragments under tutorials directory,
+# 2. Name or index of cell containing objective value (or None),
+# 3. Expected objective value.
+tutorials = [
+    (('westeros', 'westeros_baseline'),
+     'solve-objective-value', 207544.09375),
+    # on Python 2:
+    # 'solve-objective-value', 187445.953125),
+    (('westeros', 'westeros_emissions_bounds'), None, None),
+    (('westeros', 'westeros_emissions_taxes'), None, None),
+    (('westeros', 'westeros_firm_capacity'), None, None),
+    (('westeros', 'westeros_flexible_generation'), None, None),
     # FIXME use get_cell_by_name instead of assuming cell count/order is fixed
-    obs = eval(nb.cells[-13]['outputs'][0]['data']['text/plain'])
-    exp = 133105106944.0
-    assert np.isclose(obs, exp)
+    ((AT, 'austria'), -13, 133105106944.0),
+    ((AT, 'austria_single_policy'), -8, 132452155392.0),
+    ((AT, 'austria_multiple_policies'), None, None),
+    ((AT, 'austria_multiple_policies-answers'), None, None),
+    ((AT, 'austria_load_scenario'), None, None),
+]
+
+# Short, readable IDs for the tests
+ids = [arg[0][-1] for arg in tutorials]
 
 
-@pytest.mark.skipif(is_py2, reason=py2_deprecated)
-@pytest.mark.skipif(not jupyter_installed, reason=jupyter_required)
-def test_austria_single_policy():
-    fname = os.path.join(ene_path, 'austria_single_policy.ipynb')
-    nb, errors = _notebook_run(fname)
+@pytest.fixture
+def nb_path(request, tutorial_path):
+    # Combine the filename parts with the tutorial_path fixture
+    yield Path(tutorial_path, *request.param).with_suffix('.ipynb')
+
+
+# Parametrize the first 3 arguments using the variables *tutorial* and *ids*.
+# Argument 'nb_path' is indirect so that the above fixture can modify it.
+@pytest.mark.parametrize('nb_path,obj_cell,obj_value', tutorials, ids=ids,
+                         indirect=['nb_path'])
+def test_tutorial(nb_path, obj_cell, obj_value, tmp_path, tmp_env):
+    """Test tutorial in the IPython notebook at *fname*.
+
+    If *obj_cell* and *obj_value* are given, the value in the specified cell is
+    tested.
+    """
+    # The notebook can be run without errors
+    nb, errors = run_notebook(nb_path, tmp_path, tmp_env)
     assert errors == []
 
-    # FIXME use get_cell_by_name instead of assuming cell count/order is fixed
-    obs = eval(nb.cells[-8]['outputs'][0]['data']['text/plain'])
-    exp = 132452155392.0
-    assert np.isclose(obs, exp)
-
-
-@pytest.mark.skipif(is_py2, reason=py2_deprecated)
-@pytest.mark.skipif(not jupyter_installed, reason=jupyter_required)
-def test_austria_multiple_policies():
-    fname = os.path.join(ene_path, 'austria_multiple_policies.ipynb')
-    nb, errors = _notebook_run(fname)
-    assert errors == []
-
-
-@pytest.mark.skipif(is_py2, reason=py2_deprecated)
-@pytest.mark.skipif(not jupyter_installed, reason=jupyter_required)
-def test_austria_multiple_policies_answers():
-    fname = os.path.join(ene_path, 'austria_multiple_policies-answers.ipynb')
-    nb, errors = _notebook_run(fname)
-    assert errors == []
-
-
-@pytest.mark.skipif(is_py2, reason=py2_deprecated)
-@pytest.mark.skipif(not jupyter_installed, reason=jupyter_required)
-def test_austria_load():
-    fname = os.path.join(ene_path, 'austria_load_scenario.ipynb')
-    nb, errors = _notebook_run(fname)
-    assert errors == []
+    # Cell identified by obj_cell has a particular value
+    assert not obj_cell or np.isclose(get_cell_output(nb, obj_cell), obj_value)
