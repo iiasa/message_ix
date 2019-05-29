cf6dbf22079ee819e72732f19a65c404a3abb0fa
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-05-29 12:03:54 +0200

    Address @gidden comments

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -18,25 +18,27 @@ if sys.version_info[0] == 2:
 AT = 'Austrian_energy_system'
 
 # Argument values to parametrize test_tutorial
-# Each item is a 3-tuple:
+#
+# Each item is a 2-tuple:
 # 1. Path fragments under tutorials directory,
-# 2. Name or index of cell containing objective value (or None),
-# 3. Expected objective value.
+# 2. List containing 0 or more 2-tuples, each:
+#    a. Name or index of cell containing objective value,
+#    b. Expected objective value.
 tutorials = [
     (('westeros', 'westeros_baseline'),
-     'solve-objective-value', 207544.09375),
+     [('solve-objective-value', 207544.09375)]),
     # on Python 2:
     # 'solve-objective-value', 187445.953125),
-    (('westeros', 'westeros_emissions_bounds'), None, None),
-    (('westeros', 'westeros_emissions_taxes'), None, None),
-    (('westeros', 'westeros_firm_capacity'), None, None),
-    (('westeros', 'westeros_flexible_generation'), None, None),
+    (('westeros', 'westeros_emissions_bounds'), []),
+    (('westeros', 'westeros_emissions_taxes'), []),
+    (('westeros', 'westeros_firm_capacity'), []),
+    (('westeros', 'westeros_flexible_generation'), []),
     # FIXME use get_cell_by_name instead of assuming cell count/order is fixed
-    ((AT, 'austria'), -13, 133105106944.0),
-    ((AT, 'austria_single_policy'), -8, 132452155392.0),
-    ((AT, 'austria_multiple_policies'), None, None),
-    ((AT, 'austria_multiple_policies-answers'), None, None),
-    ((AT, 'austria_load_scenario'), None, None),
+    ((AT, 'austria'), [(-13, 133105106944.0)]),
+    ((AT, 'austria_single_policy'), [(-8, 132452155392.0)]),
+    ((AT, 'austria_multiple_policies'), []),
+    ((AT, 'austria_multiple_policies-answers'), []),
+    ((AT, 'austria_load_scenario'), []),
 ]
 
 # Short, readable IDs for the tests
@@ -51,17 +53,17 @@ def nb_path(request, tutorial_path):
 
 # Parametrize the first 3 arguments using the variables *tutorial* and *ids*.
 # Argument 'nb_path' is indirect so that the above fixture can modify it.
-@pytest.mark.parametrize('nb_path,obj_cell,obj_value', tutorials, ids=ids,
+@pytest.mark.parametrize('nb_path,cell_values', tutorials, ids=ids,
                          indirect=['nb_path'])
-def test_tutorial(nb_path, obj_cell, obj_value, tmp_path, tmp_env):
+def test_tutorial(nb_path, cell_values, tmp_path, tmp_env):
     """Test tutorial in the IPython notebook at *fname*.
 
-    If *obj_cell* and *obj_value* are given, the value in the specified cell is
-    tested.
+    If *cell_values* are given, values in the specified cells are tested.
     """
     # The notebook can be run without errors
     nb, errors = run_notebook(nb_path, tmp_path, tmp_env)
     assert errors == []
 
-    # Cell identified by obj_cell has a particular value
-    assert not obj_cell or np.isclose(get_cell_output(nb, obj_cell), obj_value)
+    for cell, value in cell_values:
+        # Cell identified by name or index has a particular value
+        assert np.isclose(get_cell_output(nb, cell), value)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -19,14 +19,18 @@ def test_make_df_raises():
     pytest.raises(ValueError, utils.make_df, 42, baz=[42, 42])
 
 
-def test_testing(test_mp):
-    # Check the model creation functions in testing.py
+def test_testing_make_scenario(test_mp):
+    """Check the model creation functions in message_ix.testing."""
+
+    # MESSAGE-scheme Dantzig problem can be created
     scen = message_ix.testing.make_dantzig(test_mp, True)
     assert isinstance(scen, Scenario)
 
+    # Multi-year variant can be created
     scen = message_ix.testing.make_dantzig(test_mp, solve=True,
                                            multi_year=True)
     assert isinstance(scen, Scenario)
 
+    # Westeros model can be created
     scen = message_ix.testing.make_westeros(test_mp, True)
     assert isinstance(scen, Scenario)
