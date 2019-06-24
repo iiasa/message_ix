3f9eece40c89399b2b0bdced56f79b9d2a1447fa
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-06-24 09:50:26 +0200

    Appease Stickler

--- tests/test_reporting.py
+++ tests/test_reporting.py
@@ -131,10 +131,10 @@ def test_reporter_from_westeros(test_mp):
     obs = obs.filter(variable='total om*')
     assert len(obs.data) == 9
     assert all(
-        obs['variable'] ==
-        ['total om cost|coal_ppl'] * 3 +
-        ['total om cost|grid'] * 3 +
-        ['total om cost|wind_ppl'] * 3
+        obs['variable']
+        == ['total om cost|coal_ppl'] * 3
+        + ['total om cost|grid'] * 3
+        + ['total om cost|wind_ppl'] * 3
     )
     assert all(obs['year'] == [700, 710, 720] * 3)
 
