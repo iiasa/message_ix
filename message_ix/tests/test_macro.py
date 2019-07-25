12dd446f2080563c79ac8c4b806588115f0cb15c
Matthew Gidden <matthew.gidden@gmail.com>
2019-07-25 10:51:56 +0200

    more test cleanup

--- message_ix/tests/test_macro.py
+++ message_ix/tests/test_macro.py
@@ -11,11 +11,14 @@ except ImportError:
 from message_ix import Scenario, macro
 from message_ix.testing import make_westeros
 
-msg_args = ('canning problem (MESSAGE scheme)', 'standard')
-
 # tons of deprecation warnings come from reading excel (xlrd library), ignore
 # them for now
 pytestmark = pytest.mark.filterwarnings("ignore")
+
+MSG_ARGS = ('canning problem (MESSAGE scheme)', 'standard')
+
+DATA_PATH = Path(__file__).parent / 'data' / 'westeros_macro_input.xlsx'
+
 
 @pytest.fixture(scope='module')
 def westeros_solved(test_mp):
@@ -28,7 +31,7 @@ def westeros_not_solved(test_mp):
 
 
 def test_init(test_mp):
-    scen = Scenario(test_mp, *msg_args)
+    scen = Scenario(test_mp, *MSG_ARGS)
 
     scen = scen.clone('foo', 'bar')
     scen.check_out()
@@ -45,29 +48,25 @@ def test_init(test_mp):
 
 def test_calc_valid_data_file(westeros_solved):
     s = westeros_solved
-    path = Path(__file__).parent / 'data' / 'westeros_macro_input.xlsx'
-    c = macro.Calculate(s, path)
+    c = macro.Calculate(s, DATA_PATH)
     c.read_data()
 
 
 def test_calc_valid_data_dict(westeros_solved):
     s = westeros_solved
-    path = Path(__file__).parent / 'data' / 'westeros_macro_input.xlsx'
-    data = pd.read_excel(path, sheet_name=None)
+    data = pd.read_excel(DATA_PATH, sheet_name=None)
     c = macro.Calculate(s, data)
     c.read_data()
 
 
 def test_calc_no_solution(westeros_not_solved):
     s = westeros_not_solved
-    path = Path(__file__).parent / 'data' / 'westeros_macro_input.xlsx'
-    pytest.raises(RuntimeError, macro.Calculate, s, path)
+    pytest.raises(RuntimeError, macro.Calculate, s, DATA_PATH)
 
 
 def test_calc_data_missing_par(westeros_solved):
     s = westeros_solved
-    path = Path(__file__).parent / 'data' / 'westeros_macro_input.xlsx'
-    data = pd.read_excel(path, sheet_name=None)
+    data = pd.read_excel(DATA_PATH, sheet_name=None)
     data.pop('gdp_calibrate')
     c = macro.Calculate(s, data)
     pytest.raises(ValueError, c.read_data)
@@ -75,8 +74,7 @@ def test_calc_data_missing_par(westeros_solved):
 
 def test_calc_data_missing_column(westeros_solved):
     s = westeros_solved
-    path = Path(__file__).parent / 'data' / 'westeros_macro_input.xlsx'
-    data = pd.read_excel(path, sheet_name=None)
+    data = pd.read_excel(DATA_PATH, sheet_name=None)
     # skip first data point
     data['gdp_calibrate'] = data['gdp_calibrate'].drop('year', axis=1)
     c = macro.Calculate(s, data)
@@ -85,9 +83,24 @@ def test_calc_data_missing_column(westeros_solved):
 
 def test_calc_data_missing_datapoint(westeros_solved):
     s = westeros_solved
-    path = Path(__file__).parent / 'data' / 'westeros_macro_input.xlsx'
-    data = pd.read_excel(path, sheet_name=None)
+    data = pd.read_excel(DATA_PATH, sheet_name=None)
     # skip first data point
     data['gdp_calibrate'] = data['gdp_calibrate'][1:]
     c = macro.Calculate(s, data)
     pytest.raises(ValueError, c.read_data)
+
+#
+# Regression tests: these tests were compiled upon moving from R to Python,
+# values were confirmed correct at the time and thus are tested explicitly here
+#
+
+
+def test_calc_rho(westeros_solved):
+    s = westeros_solved
+    c = macro.Calculate(s, DATA_PATH)
+    c.read_data()
+    obs = c._rho().values
+    assert len(obs) == 1
+    obs = obs[0]
+    exp = -4
+    assert obs == exp
