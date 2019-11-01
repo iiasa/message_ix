f42ad6bc16404e27b71c40cc2943998ccb6044bb
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-11-01 23:50:36 +0100

    Add testing.nightly skeleton

---
+++ message_ix/testing/nightly.py
@@ -0,0 +1,27 @@
+
+def download_db():
+    pass
+
+
+def download_license():
+    pass
+
+
+def generate_test_file():
+    pass
+
+
+def fetch_scenarios():
+    pass
+
+
+def make_db():
+    pass
+
+
+def upload_db():
+    pass
+
+
+def upload_license():
+    pass
