bae9cb5123afad4c08f0f5043c91ddf62225d8d4
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-11-01 23:58:41 +0100

    Add download_license to testing.nightly

--- message_ix/testing/nightly.py
+++ message_ix/testing/nightly.py
@@ -1,6 +1,8 @@
 import os
 import requests
+import subprocess
 import tarfile
+
 
 PASSWORD = os.environ['MESSAGE_IX_CI_PW']
 URL = 'https://data.ene.iiasa.ac.at/continuous_integration/scenario_db/'
@@ -27,7 +29,25 @@ def download_db():
 
 
 def download_license():
-    pass
+    filename = 'gamslice.txt'
+
+    print(subprocess.check_output('which gams', shell=True).strip())
+
+    localpath = os.path.dirname(
+        subprocess.check_output(['which', 'gams']).strip())
+    localpath = localpath.decode()  # py3
+
+    r = requests.get(URL + filename, auth=(USERNAME, PASSWORD))
+
+    if r.status_code == 200:
+        outpath = os.path.join(localpath, filename)
+        print('Downloading {} from {} to {}'.format(filename, URL, localpath))
+        with open(outpath, 'wb') as out:
+            for bits in r.iter_content():
+                out.write(bits)
+    else:
+        raise IOError(
+            'Failed download with user/pass: {}/{}'.format(USERNAME, PASSWORD))
 
 
 def generate_test_file():
