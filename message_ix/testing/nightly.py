4da09cf6c3fa771e48ddb6bad921811da04bd8eb
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-11-01 23:54:10 +0100

    Add download_db to testing.nightly

--- message_ix/testing/nightly.py
+++ message_ix/testing/nightly.py
@@ -1,6 +1,29 @@
+import os
+import requests
+import tarfile
+
+PASSWORD = os.environ['MESSAGE_IX_CI_PW']
+URL = 'https://data.ene.iiasa.ac.at/continuous_integration/scenario_db/'
+USERNAME = os.environ['MESSAGE_IX_CI_USER']
+FILENAME = 'db.tar.gz'
+
 
 def download_db():
-    pass
+    r = requests.get(URL + FILENAME, auth=(USERNAME, PASSWORD))
+
+    if r.status_code == 200:
+        print('Downloading {} from {}'.format(FILENAME, URL))
+        with open(FILENAME, 'wb') as out:
+            for bits in r.iter_content():
+                out.write(bits)
+        print('Untarring {}'.format(FILENAME))
+        tar = tarfile.open(FILENAME, "r:gz")
+        tar.extractall()
+        tar.close()
+        os.remove(FILENAME)
+    else:
+        raise IOError(
+            'Failed download with user/pass: {}/{}'.format(USERNAME, PASSWORD))
 
 
 def download_license():
