88613cdac831d7d6bdcf15587f161e03749e4ba8
Clara <clara.orthofer@tum.de>
2019-11-20 17:15:52 +0100

    add code to get all parameters related to a technology from database

---
+++ message_ix/tools/get_tech_data/__main__.py
@@ -0,0 +1,33 @@
+"""Get all parameters related to a certain technology/commodity/year ect. from the data base
+
+"""
+from typing import Optional
+
+import ixmp
+import message_ix
+
+from . import get_tech_description
+
+
+def main(model_ref: str, scen_ref: str, filters: dict, filename: str, key_filter:Optional[str] ='technology',
+         version_ref: Optional[int] = None):
+    print('>> message_ix.tools.get_tech_data...')
+
+    # Handle default arguments
+    ref_kw = dict(model=model_ref, scen=scen_ref)
+    if version_ref:
+        ref_kw['version'] = version_ref
+
+    # Load the ixmp Platform
+    mp = ixmp.Platform(dbtype='HSQLDB')
+
+    # Loading the reference scenario
+    base = message_ix.Scenario(mp, **ref_kw)
+
+    # Calling the main function
+    get_tech_description(base=base, key_filter=key_filter, filters=filters, filename=filename)
+
+    mp.close_db()
+
+# Execute the script
+main()
