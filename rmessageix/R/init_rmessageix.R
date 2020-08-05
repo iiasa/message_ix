96de6e3bb9bfb09e9b320c92cd70800ba3dc453c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-08-05 16:30:20 +0200

    Remove outdated content from init_rmessageix.R

--- rmessageix/R/init_rmessageix.R
+++ rmessageix/R/init_rmessageix.R
@@ -14,28 +14,9 @@ NULL
             utils::packageDescription(pkgname)$Version) )
 }
 
-utils::globalVariables(c("ixmp_path"))
+message_ix <- NULL
 
 .onLoad <- function(libname, pkgname) {
-
-  ## Set path ixmp folder in message_ix working copy
-  ixmp_path <<- Sys.getenv("IXMP_PATH")
-
-  if(ixmp_path == "")
-    warning("Check MESSAGE model installation")
-  
-  # careful to have all the slashes in the same direction
-  model_file = gsub("/","\\\\" ,file.path( paste(ixmp_path, "\\message_ix\\model", sep = '') , "{model}_run.gms" ) )
-  inp = gsub("/","\\\\" , file.path( paste(ixmp_path, "\\message_ix\\model\\data", sep = '') , "MsgData_{case}.gdx" ) )
-  outp = gsub("/","\\\\" , file.path( paste(ixmp_path, "\\message_ix\\model\\output", sep = '') , "MsgOutput_{case}.gdx" ) )
-  iter_file = gsub("/","\\\\" , file.path( paste(ixmp_path, "\\message_ix\\model\\output", sep = '') , "MsgIterationReport_{case}.gdx" ) )
-  solve_args = paste("--in=",inp," --out=",outp," --iter=", iter_file, sep = '')
-  
-  for (i in c("MESSAGE","MESSAGE-MACRO")) {
-    ModelConfig[[i]] <<- list(model_file = model_file,
-                             inp = inp,
-                             outp = outp,
-                             args = solve_args)
-  }
-
+  # Set message_ix in the global namespace
+  message_ix <<- reticulate::import("message_ix", delay_load = TRUE)
 }
