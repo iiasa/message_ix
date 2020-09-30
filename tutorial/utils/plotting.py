e7a3374c2a364008e8ea6e14acb03ef70d84fb57
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-09-30 16:16:55 +0200

    Tease reporting API in westeros_baseline.ipynb

--- tutorial/utils/plotting.py
+++ tutorial/utils/plotting.py
@@ -38,54 +38,55 @@ def stacked_bar(qty, dims=["nl", "t", "ya"], units="", title="", cf=1.):
     ).legend(loc="center left", bbox_to_anchor=(1., 0.5))
 
 
-class Plots:
-    """Plots for tutorial energy models."""
+PLOTS = [
+    ("activity", stacked_bar, "out:nl-t-ya", "GWa"),
+    ("capacity", stacked_bar, "CAP:nl-t-ya", "GW"),
+    ("prices", stacked_bar, "PRICE_COMMODITY:n-c-y", "¢/kW·h"),
+]
+
+
+def prepare_tutorial_plots(rep: Reporter, input_costs="$/GWa") -> None:
+    """Prepare `rep` to Plots for tutorial energy models.
+
+    Makes available several keys:
+
+    - `plot activity`
+    - `plot capacity`
+    - `plot prices`
+
+    To control the contents of each plot, use :meth:`.set_filters` on `rep`.
+    """
     # TODO re-add plot_new_capacity()
     # TODO re-add plot_extraction()
     # TODO re-add plot_fossil_supply_curve()
 
-    def __init__(self, scenario, input_costs='$/GWa'):
-        # Conversion factors between input units and plotting units
-        # TODO use the correct units in tutorials
-        # TODO allow the correct units to pass through reporting
-        cost_unit_conv = {
-            "$/GWa": 1.,
-            "$/MWa": 1e3,
-            "$/kWa": 1e6,
-        }.get(input_costs)
+    # Conversion factors between input units and plotting units
+    # TODO use exact units in all tutorials
+    # TODO allow the correct units to pass through reporting
+    cost_unit_conv = {
+        "$/GWa": 1.,
+        "$/MWa": 1e3,
+        "$/kWa": 1e6,
+    }.get(input_costs)
 
-        # Basic setup of the Reporter
-        self.rep = Reporter.from_scenario(scenario)
-        self.rep.configure(units={"replace": {"-": ""}})
+    # Basic setup of the reporter
+    rep.configure(units={"replace": {"-": ""}})
 
-        # Add one node for each plot
-        for title, key_str, units in [
-            ("activity", "out:nl-t-ya", "GWa"),
-            ("capacity", "CAP:nl-t-ya", "GW"),
-            ("prices", "PRICE_COMMODITY:n-c-y", "¢/kW·h"),
-        ]:
-            # Convert the string to a Key object
-            key = Key.from_str_or_key(key_str)
+    # Add one node to the reporter for each plot
+    for title, func, key_str, units in PLOTS:
+        # Convert the string to a Key object so as to reference its .dims
+        key = Key.from_str_or_key(key_str)
 
-            # Operation for the reporter: generate a stacked bar plot
-            comp = partial(
-                stacked_bar,
-                dims=key.dims,
-                units=units,
-                title=title,
-                cf=1. if title != "Prices" else (cost_unit_conv * 100 / 8760)
-            )
-            # Add the computation under a key like "plot activity"
-            self.rep.add(f"plot {title}", (comp, key))
+        # Operation for the reporter
+        comp = partial(
+            # The function to use, e.g. stacked_bar()
+            func,
+            # Other keyword arguments to the plotting function
+            dims=key.dims,
+            units=units,
+            title=title,
+            cf=1. if title != "Prices" else (cost_unit_conv * 100 / 8760)
+        )
 
-    def plot_output(self, tecs):
-        self.rep.set_filters(t=[tecs] if isinstance(tecs, str) else tecs)
-        return self.rep.get("plot activity")
-
-    def plot_capacity(self, tecs):
-        self.rep.set_filters(t=[tecs] if isinstance(tecs, str) else tecs)
-        return self.rep.get("plot capacity")
-
-    def plot_prices(self, cmdty):
-        self.rep.set_filters(c=[cmdty] if isinstance(cmdty, str) else cmdty)
-        return self.rep.get("plot prices")
+        # Add the computation under a key like "plot activity"
+        rep.add(f"plot {title}", (comp, key))
--- tutorial/westeros/tools.py
+++ tutorial/westeros/tools.py
@@ -3,4 +3,6 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parents[1] / "utils"))
 
-from plotting import Plots  # noqa: E402, F401
+from plotting import prepare_tutorial_plots  # noqa: E402
+
+__all__ = ["prepare_tutorial_plots"]
