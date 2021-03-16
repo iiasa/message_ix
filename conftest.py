a7b8f93782cab8cb022bc687d89b0e54e9667a6b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-03-16 21:08:34 +0100

    Adjust default CPLEX thread count on GitHub Actions

--- conftest.py
+++ conftest.py
@@ -1,2 +1,15 @@
+import os
+
 # Use the fixtures test_mp, test_mp_props, and tmp_env from ixmp.testing
 pytest_plugins = ["ixmp.testing"]
+
+# Hooks
+
+
+def pytest_sessionstart(session):
+    """Use only 2 threads for CPLEX on GitHub Actions runners with 2 CPU cores."""
+    import message_ix.models
+
+    if "GITHUB_ACTIONS" in os.environ:
+        message_ix.models.DEFAULT_CPLEX_OPTIONS["threads"] = 2
+
