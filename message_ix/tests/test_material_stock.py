9fcf5eec0b35f19ee6902c17f492ad7db424a870
Jihoon <min@iiasa.ac.at>
2021-04-01 11:44:30 +0200

    Re-format in 'black' style

--- message_ix/tests/test_material_stock.py
+++ message_ix/tests/test_material_stock.py
@@ -30,10 +30,20 @@ def test_new_params_defined(test_mp):
     assert result.shape == (1, 9)
 
     # Check correct behaviour when adding key-worded args:
-    defaults = dict(node_loc="World", node_dest="World", technology="coal_ppl",
-                    node_origin="World",
-                    commodity="coal", level="end_of_life", year_vtg=2020, time="year",
-                    time_origin="year", time_dest="year", value=1, unit="t")
+    defaults = dict(
+        node_loc="World",
+        node_dest="World",
+        technology="coal_ppl",
+        node_origin="World",
+        commodity="coal",
+        level="end_of_life",
+        year_vtg=2020,
+        time="year",
+        time_origin="year",
+        time_dest="year",
+        value=1,
+        unit="t",
+    )
 
     result_i = make_df("input_cap_ret", **defaults)
     pdt.assert_series_equal(result_i["commodity"], pd.Series("coal", name="commodity"))
@@ -50,25 +60,40 @@ def test_new_params_working(test_mp):
     scen = make_westeros(test_mp, solve=False)
 
     # Fake conversion of electricity into steel at coal_ppl
-    df_output = dict(node_loc="Westeros", node_dest="Westeros", technology="coal_ppl",
-                    node_origin="Westeros",
-                    commodity="steel", level="material", year_vtg=[690, 700, 710, 720],
-                    time="year",
-                    time_origin="year", time_dest="year", value=1, unit="t")
+    df_output = dict(
+        node_loc="Westeros",
+        node_dest="Westeros",
+        technology="coal_ppl",
+        node_origin="Westeros",
+        commodity="steel",
+        level="material",
+        year_vtg=[690, 700, 710, 720],
+        time="year",
+        time_origin="year",
+        time_dest="year",
+        value=1,
+        unit="t",
+    )
 
     result_o = make_df("output_cap_new", **df_output)
 
     # Add steel demand
-    df_demand = dict(node="Westeros",
-                    commodity="steel", level="material", year=[700, 710, 720],
-                    time="year", value=[100, 150, 170], unit="t")
+    df_demand = dict(
+        node="Westeros",
+        commodity="steel",
+        level="material",
+        year=[700, 710, 720],
+        time="year",
+        value=[100, 150, 170],
+        unit="t",
+    )
 
     result_d = make_df("demand", **df_demand)
 
     # Add new commodity and level
     scen.check_out()
-    scen.add_set('commodity', 'steel')
-    scen.add_set('level', 'material')
+    scen.add_set("commodity", "steel")
+    scen.add_set("level", "material")
     scen.commit("add steel as a commodity")
 
     # Add new parameters
@@ -83,6 +108,6 @@ def test_new_params_working(test_mp):
 
     scen.solve()
 
-    price_steel = scen.var('PRICE_COMMODITY', {'commodity':'steel'})
+    price_steel = scen.var("PRICE_COMMODITY", {"commodity": "steel"})
 
     assert np.isclose(price_steel.lvl[0], 536.519)
