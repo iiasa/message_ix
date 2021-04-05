d76d76c9036a146eccf93b73871e6d3523c76cdc
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-04-05 20:32:26 +0200

    Clean up prior to merge

--- conftest.py
+++ conftest.py
@@ -20,10 +20,3 @@ def pytest_report_header(config, startdir):
     import message_ix
 
     return f"message_ix location: {Path(message_ix.__file__).parent}"
-
-
-def pytest_unconfigure(config):
-    import jpype
-
-    if jpype.isJVMStarted():
-        jpype.shutdownJVM()
