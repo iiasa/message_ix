baf94ddb90d8f2baf6ce9135f4bee6919fadf220
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-03-10 13:29:32 +0100

    Precisely match names of GAMS runtime directories

--- message_ix/cli.py
+++ message_ix/cli.py
@@ -42,7 +42,7 @@ def copy_model(path, overwrite, set_default):
     src_dir = Path(__file__).parent / "model"
     for src in src_dir.rglob("*.*"):
         # Skip certain files
-        if src.suffix in (".gdx", ".log", ".lst") or re.search("225", str(src)):
+        if src.suffix in (".gdx", ".log", ".lst") or re.search("225[a-z]+", str(src)):
             continue
 
         # Destination path
