86f62c9b0362cf5e6aa371b2c424c6968c8a9dd8
Ben <behnam.zakeri@live.com>
2022-12-12 16:20:20 +0100

    Update doc and test_reporting for the new tutorial

--- message_ix/tests/test_tutorials.py
+++ message_ix/tests/test_tutorials.py
@@ -55,6 +55,7 @@ tutorials: List[Tuple] = [
     (("westeros", "westeros_soft_constraints"), [], {}),
     (("westeros", "westeros_addon_technologies"), [], {}),
     (("westeros", "westeros_historical_new_capacity"), [], {}),
+    (("westeros", "westeros_multinode"), [], {}),
     # NB this is the same value as in test_reporter()
     (("westeros", "westeros_report"), [("len-rep-graph", 13074)], {}),
     ((AT, "austria"), [("solve-objective-value", 206321.90625)], {}),
