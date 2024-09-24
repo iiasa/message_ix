705a49fc6b9e02f274dd82e20e032f55bf81a906
Fridolin Glatter <glatter@iiasa.ac.at>
2024-09-24 13:13:26 +0200

    Refactor map_for_sankey and corresponding test

--- message_ix/util/sankey.py
+++ message_ix/util/sankey.py
@@ -1,28 +1,70 @@
-from typing import Any, Optional
+from typing import Any, List, LiteralString, Optional, Tuple, Union
 
-import pandas as pd
+from pyam import IamDataFrame
 
 try:
-    from pyam.str import get_variable_components as gvc
+    from pyam.str import get_variable_components
 except ImportError:  # Python < 3.10, pandas < 2.0
-    from pyam.utils import get_variable_components as gvc
+    from pyam.utils import get_variable_components
 
 
-def sankey_mapper(
-    df: pd.DataFrame,
+def map_for_sankey(
+    iam_df: IamDataFrame,
     year: int,
     region: str,
     exclude: list[Optional[str]] = [],
-) -> dict[str, Any]:
-    mapping = {}
+) -> dict[str, Tuple[Union[List, Any, LiteralString], Union[List, Any, LiteralString]]]:
+    """Maps input to output flows to enable Sankey plots.
 
-    for var in df.filter(region=region + "*", year=year).variable:
-        is_input = gvc(var, 0) == "in"
-        (start_idx, end_idx) = ([1, 2], [3, 4]) if is_input else ([3, 4], [1, 2])
-        source = gvc(var, start_idx, join=True)
-        target = gvc(var, end_idx, join=True)
-        if source in exclude or target in exclude:
-            continue
-        mapping[var] = (source, target)
+    Parameters
+    ----------
+    iam_df: :class:`pyam.IamDataframe`
+        The IAMC-format DataFrame holding the data to plot as Sankey diagrams.
+    year: int
+        The year to display in the Sankey diagram.
+    region: str
+        The region to display in the Sankey diagram.
+    exclude: list[str], optional
+        If provided, exclude these keys from the Sankey diagram. Defaults to an empty
+        list, i.e. showing all flows.
 
-    return mapping
+    Returns
+    -------
+    mapping: dict
+        A mapping from variable names to their inputs and outputs.
+    """
+    return {
+        var: get_source_and_target(var)
+        for var in iam_df.filter(region=region + "*", year=year).variable
+        if not exclude_flow(get_source_and_target(var), exclude)
+    }
+
+
+def get_source_and_target(
+    variable: str,
+) -> Tuple[Union[List, Any, LiteralString], Union[List, Any, LiteralString]]:
+    """Get source and target for the `variable` flow."""
+    start_idx, end_idx = set_start_and_end_index(variable)
+    return (
+        get_variable_components(variable, start_idx, join=True),
+        get_variable_components(variable, end_idx, join=True),
+    )
+
+
+def set_start_and_end_index(variable: str) -> Tuple[List[int], List[int]]:
+    """Get indices of source and target in variable name."""
+    return (
+        ([1, 2], [3, 4])
+        if get_variable_components(variable, 0) == "in"
+        else ([3, 4], [1, 2])
+    )
+
+
+def exclude_flow(
+    flow: Tuple[Union[List, Any, LiteralString], Union[List, Any, LiteralString]],
+    exclude: List[Optional[str]],
+) -> bool:
+    """Exclude sources or targets of variable flow if requested."""
+    if flow[0] in exclude or flow[1] in exclude:
+        return True
+    return False
