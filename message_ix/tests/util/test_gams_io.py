0ea54dd91a05da3d732afb7565949a3387f8efe7
Fridolin Glatter <glatter@iiasa.ac.at>
2025-05-07 13:16:02 +0200

    Avoid ixmp4 imports for Python 3.9

--- message_ix/tests/util/test_gams_io.py
+++ message_ix/tests/util/test_gams_io.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import pytest
 from ixmp import Platform
-from ixmp.util.ixmp4 import ContainerData
+from ixmp.testing import min_ixmp4_version
 
 from message_ix import Scenario
 from message_ix.testing import make_westeros
-from message_ix.util.gams_io import (
-    add_auxiliary_items_to_container_data_list,
-    add_default_data_to_container_data_list,
-    store_message_version,
-)
 from message_ix.util.scenario_data import HELPER_INDEXSETS, HELPER_TABLES
+
+pytestmark = min_ixmp4_version
 
 
 def test_store_message_version() -> None:
+    from ixmp.util.ixmp4 import ContainerData
+
+    from message_ix.util.gams_io import store_message_version
+
     data: list[ContainerData] = []
     store_message_version(container_data=data)
     expected_names = ["version_part", "MESSAGE_ix_version"]
@@ -23,6 +24,10 @@ def test_store_message_version() -> None:
 
 @pytest.mark.ixmp4
 def test_add_default_data_to_container_data_list(test_mp: Platform) -> None:
+    from ixmp.util.ixmp4 import ContainerData
+
+    from message_ix.util.gams_io import add_default_data_to_container_data_list
+
     scenario = Scenario(mp=test_mp, model="model", scenario="scenario", version="new")
 
     # Test with empty 'technology' set
@@ -64,6 +69,10 @@ def test_add_default_data_to_container_data_list(test_mp: Platform) -> None:
 def test_add_auxiliary_items_to_container_data_list(
     test_mp: Platform, request: pytest.FixtureRequest
 ) -> None:
+    from ixmp.util.ixmp4 import ContainerData
+
+    from message_ix.util.gams_io import add_auxiliary_items_to_container_data_list
+
     scenario = make_westeros(mp=test_mp, emissions=True, solve=False, request=request)
     data: list[ContainerData] = []
 
