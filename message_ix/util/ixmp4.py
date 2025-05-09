10c28485cb2c0d89f344903207b7ee127bd725fd
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-05-09 14:56:53 +0200

    Add .util.ixmp4.on_ixmp4backend()
    Simplify repeated conditionals in .core.Scenario.

--- message_ix/util/ixmp4.py
+++ message_ix/util/ixmp4.py
@@ -1,4 +1,23 @@
-from typing import Any, Literal
+from typing import Any, Literal, Union
+
+import ixmp
+import ixmp.backend
+
+
+def on_ixmp4backend(obj: Union["ixmp.Platform", "ixmp.TimeSeries"]) -> bool:
+    """:any:`True` if `ts` is stored on :class:`IXMP4Backend`.
+
+    .. todo:: Move upstream, to :mod:`ixmp`.
+    """
+    if "ixmp4" not in ixmp.backend.available():
+        return False
+
+    from ixmp.backend.ixmp4 import IXMP4Backend
+
+    return isinstance(
+        (obj if isinstance(obj, ixmp.Platform) else obj.platform)._backend,
+        IXMP4Backend,
+    )
 
 
 def add_or_extend_item_list(
