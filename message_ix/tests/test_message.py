225bcc41ed353bfa1b157ad6e659acf6273da797
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2026-09-18 13:03:34 +0200

    Add a test for bound_activity_lo

--- message_ix/tests/test_message.py
+++ message_ix/tests/test_message.py
@@ -6,7 +6,9 @@ import ixmp
 import pytest
 from ixmp.backend.jdbc import JDBCBackend
 
+from message_ix import make_df
 from message_ix.message import MESSAGE
+from message_ix.testing import make_dantzig
 
 if TYPE_CHECKING:
     from ixmp import Platform
@@ -18,7 +20,7 @@ pytestmark = pytest.mark.ixmp4_209
 class TestMESSAGE:
     """Tests of :class:`.MESSAGE`."""
 
-    def test_initialize(self, caplog, test_mp: "Platform") -> None:
+    def test_initialize(self, test_mp: "Platform") -> None:
         # Expected numbers of items by type
         exp = defaultdict(list)
         for name, spec in MESSAGE.items.items():
@@ -40,7 +42,9 @@ class TestMESSAGE:
             obs_names = getattr(s, f"{ix_type}_list")()
             assert sorted(obs_names) == sorted(exp_names)
 
-    def test_initialize_filter_log(self, caplog, test_mp: "Platform") -> None:
+    def test_initialize_filter_log(
+        self, caplog: pytest.LogCaptureFixture, test_mp: "Platform"
+    ) -> None:
         """Test :meth:`MESSAGE.initialize` logging under some conditions.
 
         For :class:`.Scenario` created with message_ix v3.10 or earlier, equations and
@@ -76,3 +80,35 @@ class TestMESSAGE:
         )
         extra = list(filter(message_pattern.match, caplog.messages))
         assert not extra, f"{len(extra)} unwanted log messages: {extra}"
+
+    def test_bound_activity_lo(
+        self, request: pytest.FixtureRequest, test_mp: "Platform"
+    ) -> None:
+        """Test setting of ``ACT.lo`` via ``bound_activity_lo``."""
+        scenario = make_dantzig(test_mp, solve=False, request=request)
+
+        # Modify scenario to set:
+        # - A very high var_cost for one technology.
+        # - A negative bound_activity_lo for that technology.
+        #
+        # This forces the solver to choose a negative ACT, and limits the magnitude.
+        common = dict(
+            node_loc="seattle",
+            technology="transport_from_seattle",
+            year_act=1963,
+            year_vtg=1963,
+            mode="to_chicago",
+            time="year",
+            unit="-",
+        )
+
+        with scenario.transact("test_bound_activity_lo"):
+            b_a_l = "bound_activity_lo"
+            scenario.add_par(b_a_l, make_df(b_a_l, **common, value=-100))
+            scenario.add_par("var_cost", make_df("var_cost", **common, value=1e6))
+
+        scenario.solve()
+
+        # The solver chooses the constrained value for ACT
+        common.pop("unit")
+        assert -100 == scenario.var("ACT", filters=common)["lvl"].item()
